724a39976be493c3a6233937ab0a48ebce98b161
shubhayanmastermind <121972590+shubhayanmastermind@users.noreply.github.com>
2023-03-28 22:07:49 +0530

    Add files via upload

---
+++ tracker_white_path.ino
@@ -0,0 +1,109 @@
+//left motor has more SPEED than right motor
+//track will be white path in black surface
+#define left1 10
+#define left2 11
+#define right2 5
+#define right1 6
+int x,y,z,p,q; //white--0, black--1
+void setup() {
+  //This sets frequecny as 7812.5 hz.
+  //TCCR0B = TCCR0B & B11111000 | B00000010 ;
+  
+  pinMode(left1,OUTPUT);//left motor1 input1
+  pinMode(left2,OUTPUT);//left motor1 input2
+  pinMode(right2,OUTPUT);//right motor2 input1
+  pinMode(right1,OUTPUT);//right motor2 input1
+  pinMode(12,OUTPUT);//right enable
+  pinMode(9,OUTPUT);//left enable
+  pinMode(2,INPUT);//ir1
+  pinMode(3,INPUT);//ir2
+  pinMode(4,INPUT);//ir3--middle one
+  pinMode(7,INPUT);//ir4--extreme left
+  //pinMode(13,OUTPUT);//ir4 vcc
+  pinMode(A4,INPUT);//ir5 -- extreme right
+  //pinMode(A1,INPUT);//ir5 ground
+
+  pinMode(13,OUTPUT);//blue 
+  pinMode(8,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(9,HIGH);
+  digitalWrite(12,HIGH);
+  digitalWrite(8,LOW);//led grnd
+ // digitalWrite(13,HIGH);//vcc for ir4
+  //digitalWrite(A1,LOW);//grnd for ir5
+  
+  x=digitalRead(2);//left ir
+  y=digitalRead(3);//right ir
+  z=digitalRead(4);//middle ir
+  p=digitalRead(7);//extreme left ir
+  q=digitalRead(A4);//extreme right ir
+  Serial.println(x);
+  Serial.println(y);
+  Serial.println(z);
+  
+ // go straight 
+  if(x==1 && y==1 && z==0)
+{
+  analogWrite(left1,220);
+  analogWrite(left2,0);
+   
+  analogWrite(right2,250);
+  analogWrite(right1,0);
+   //delay(20);
+  }
+else if(x==1 && y==1 && z==1)
+{
+//  digitalWrite(green,HIGH);
+  //delay(50);
+  analogWrite(left1,220);
+  analogWrite(left2,0);
+   
+  analogWrite(right2,250);
+  analogWrite(right1,0);
+  //delay(20);
+  }
+// stop
+  if(x==1 && y==1 && z==1 && p==1 && q==1){
+   analogWrite(left1,0);
+   analogWrite(left2,0);
+   
+   analogWrite(right2,0);
+   analogWrite(right1,0);
+  
+}
+//right turn
+if( y==0 || q==0){
+   analogWrite(left1,220);
+   analogWrite(left2,0);
+   
+   analogWrite(right2,0);
+   analogWrite(right1,0);
+  
+}
+// left turn
+if(x==0 || p==0){
+   analogWrite(left1,0);
+   analogWrite(left2,0);
+   
+   analogWrite(right2,250);
+   analogWrite(right1,0);
+  }
+
+// speed slow
+if(x==0 && y==0 && z==0 && p==0 && q==0){
+
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  
+   analogWrite(left1,180);
+   analogWrite(left2,0);
+   
+   analogWrite(right2,150);
+   analogWrite(right1,0);
+ }
+ 
+}
